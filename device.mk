959ad848b38d61455025e42e32722150186f46fd
Michael Bestas <mkbestas@lineageos.org>
2023-05-04 01:19:43 +0300

    gauguin: Build GPS from source
    Change-Id: I0937d1340b7d3a931e6faa4e6b4d5c7e189519d2

--- device.mk
+++ device.mk
@@ -190,8 +190,21 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.1.vendor \
-    android.hardware.gnss@2.1.vendor
+    android.hardware.gnss@2.1-impl-qti \
+    android.hardware.gnss@2.1-service-qti \
+    gnss@2.0-base.policy \
+    gnss@2.0-xtra-daemon.policy \
+    gnss@2.0-xtwifi-client.policy \
+    gnss@2.0-xtwifi-inet-agent.policy \
+    libbatching \
+    libgeofencing \
+    libgnss \
+    libgnsspps \
+    libloc_socket
+
+PRODUCT_PACKAGES += \
+    flp.conf \
+    gps.conf
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/gps/etc/,$(TARGET_COPY_OUT_VENDOR)/etc)
