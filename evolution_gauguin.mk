9fb420e12cb2ca5d1d9a74ed19a65bd7e7d13487
Adalat222 <kumaradalat2325@gmail.com>
2023-06-17 18:16:01 +0530

    gauguin: Bring up Evolution-X

--- evolution_gauguin.mk
+++ evolution_gauguin.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The Evolution-X Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,13 +11,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from gauguin device
 $(call inherit-product, device/xiaomi/gauguin/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Evolution stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_gauguin
+PRODUCT_NAME := evolution_gauguin
 PRODUCT_DEVICE := gauguin
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+#Some Evo-X stuff
+BUILD_USERNAME := Dev
+BUILD_HOSTNAME := Evolution-X
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_MINI_GAPPS := true
+TARGET_ENABLE_BLUR := false
